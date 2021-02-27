9c69d0d6e919d68b1bb441bb4c6308b621430142
mohehexd <dumblemoe@gmail.com>
2021-02-27 16:47:13 -0600

    Setting up redux and thunk middleware.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,29 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
 // import {BrowserRouter as Router} from 'react-router-dom'
-// import {createStore, compose, applyMiddleware, combineReducers} from 'redux'
 // import haikuReducer from './reducers/haikuReducer'
 // import { composeWithDevTools } from 'redux-devtools-extension'
-// import {Provider} from 'react-redux'
-// import thunk from "redux-thunk"
-
 // const initialState = {}
-// const store = createStore(haikuReducer, initialState, compose(applyMiddleware(thunk), composeWithDevTools()))
 
-// ReactDOM.render(
-//   <Router>
-//     <Provider store={store}>
-//       <App/>
-//       </Provider>
-//   </Router>,
-//   document.getElementById('root')
-// );
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import thunk  from "redux-thunk"
+
+const users = () => []
+const reducer = combineReducers({
+  users
+})
+
+const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+
+const store = createStore(reducer, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <App/>
+      </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
